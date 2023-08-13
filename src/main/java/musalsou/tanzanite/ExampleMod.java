dc3701eb407b4585e26cb5ff5c64a076a998757e
musalsouqi <musalsouqi@gmail.com>
2023-08-13 02:23:44 -0500

    initial commit

---
+++ src/main/java/musalsou/tanzanite/ExampleMod.java
@@ -0,0 +1,60 @@
+package musalsou.tanzanite;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static musalsou.tanzanite.RegisterItems.*;
+
+public class ExampleMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("tanzanite");
+
+	public static final Item CUSTOM_ITEM = new SarahsRockTwo(new FabricItemSettings().maxCount(64));
+	public static final Block CUSTOM_ORE  = new Block(FabricBlockSettings.create().strength(4.0f).requiresTool());
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("tutorial","custom_ore"));
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+			content.add(CUSTOM_ITEM);
+		});
+		Registry.register(Registries.BLOCK, new Identifier("tutorial", "custom_ore"), CUSTOM_ORE);
+
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
+		RegisterItems.register();
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.add(CUSTOM_MATERIAL_HELMET);
+		});
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.add(CUSTOM_MATERIAL_CHESTPLATE);
+		});
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.add(CUSTOM_MATERIAL_LEGGINGS);
+		});
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.add(CUSTOM_MATERIAL_BOOTS);
+		});
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/musalsou/tanzanite/RegisterItems.java
@@ -0,0 +1,24 @@
+package musalsou.tanzanite;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class RegisterItems {
+
+    public static final ArmorMaterial CUSTOM_ARMOR_MATERIAL = new PurpleArmorMaterial();
+    // If you made a new material, this is where you would note it.
+    public static final Item CUSTOM_MATERIAL_HELMET = new ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings());
+    public static final Item CUSTOM_MATERIAL_CHESTPLATE = new ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.CHESTPLATE, new Item.Settings());
+    public static final Item CUSTOM_MATERIAL_LEGGINGS = new ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.LEGGINGS, new Item.Settings());
+    public static final Item CUSTOM_MATERIAL_BOOTS = new ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.BOOTS, new Item.Settings());
+    public static void register() {
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_material_helmet"), CUSTOM_MATERIAL_HELMET);
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_material_chestplate"), CUSTOM_MATERIAL_CHESTPLATE);
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_material_leggings"), CUSTOM_MATERIAL_LEGGINGS);
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_material_boots"), CUSTOM_MATERIAL_BOOTS);
+    }
+}
